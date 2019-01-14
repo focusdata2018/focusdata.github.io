f419e0cc55ba33e54e77a823402b7c5a1191119e
FocusData <kirillvz@yandex.ru>
2019-01-14 07:01:09 +0300

    Update load_data_to_file.sh

--- agent/kulon912/load_data_to_file.sh
+++ agent/kulon912/load_data_to_file.sh
@@ -1,2 +1,8 @@
 #!/system/bin/sh
-echo whoami;
+cd /storage/sdcard0/agent912
+[ -f "/storage/sdcard0/agent912/src_data.cfg" ] || ./wget -U Mozilla -O /storage/sdcard0/agent912/src_data.cfg https://github.com/focusdata2018/focusdata2018.github.io/blob/master/agent/kulon912/src_data.cfg;
+[ -d "/storage/sdcard0/agent912/data" ] || mkdir ./data;
+for URL in `cat ./src_data.cfg`; 
+do echo $URL;
+./wget -U Mozilla -O ./data/output__$("%m_%d_%Y__%H_%M_%S").txt $URL
+done
