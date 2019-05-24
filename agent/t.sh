ebf26c29e09699df179110f11573a4c496c1f03e
FocusData <kirillvz@yandex.ru>
2019-05-24 03:59:13 +0300

    Update t.sh

--- agent/t.sh
+++ agent/t.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-https://raw.githubusercontent.com/focusdata2018/focusdata2018.github.io/master/agent/senddata.sh
+
+remote_file="https://raw.githubusercontent.com/focusdata2018/focusdata2018.github.io/master/agent/senddata.sh" 
+local_file="/yandexdisk/focusdata/agent/k912_1/senddata.sh" 
+
+fuction download_scripts()
+{
+curl -o $local_file $remote_file
+cd /yandexdisk/focusdata/agent/k912_1
+}
+
+download_scripts
+chmod 777 $local_file
+
+exec $local_file
