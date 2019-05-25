b7ef071beca630bb546e1276e68cc91c8d9572f5
FocusData <kirillvz@yandex.ru>
2019-05-25 07:30:36 +0300

    Update

--- agent/readweb.sh
+++ agent/readweb.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+function getfilesize(){
+local file=""
+if [[ "$1" ]]; then 
+file="$1"
+else
+echo "File unknown in function getfilesize"
+exit 1
+fi
+(
+  du --apparent-size --block-size=1 "$file" 2>/dev/null ||
+  gdu --apparent-size --block-size=1 "$file" 2>/dev/null ||
+  find "$file" -printf "%s" 2>/dev/null ||
+  gfind "$file" -printf "%s" 2>/dev/null ||
+  stat --printf="%s" "$file" 2>/dev/null ||
+  stat -f%z "$file" 2>/dev/null ||
+  wc -c <"$file" 2>/dev/null
+) | awk '{print $1}'
+}
+
 function get_http(){
 DEST=""
 if [[ "$1" ]]; then 
