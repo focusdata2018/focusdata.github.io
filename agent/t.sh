45134491acf1af202ad8b81eae3a753e1bbfae9a
FocusData <kirillvz@yandex.ru>
2019-05-26 08:37:50 +0300

    Update

--- agent/t.sh
+++ agent/t.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+clear
+#https://pingvinus.ru/note/ps-kill-killall
+killall -s 9 senddata.sh
+killall -s 9 readweb.sh
 
+echo "Download senddata.sh"
 remote_file="https://raw.githubusercontent.com/focusdata2018/focusdata2018.github.io/master/agent/senddata.sh" 
 local_file="/yandexdisk/focusdata/agent/k912_1/senddata.sh" 
-
 download_scripts()
 {
-curl -o $local_file $remote_file
-cd /yandexdisk/focusdata/agent/k912_1
+    curl -s -o $local_file $remote_file
+    cd /yandexdisk/focusdata/agent/k912_1
 }
-
 download_scripts
-chmod 777 $local_file
+chmod 777 $local_file 
+echo "Run senddata.sh in backgroud"
+exec $local_file&
 
-exec $local_file
+echo "Download readweb.sh"
+remote_file="https://raw.githubusercontent.com/focusdata2018/focusdata2018.github.io/master/agent/readweb.sh" 
+local_file="/yandexdisk/focusdata/agent/k912_1/readweb.sh" 
+download_scripts()
+{
+    curl -s -o $local_file $remote_file
+    cd /yandexdisk/focusdata/agent/k912_1
+}
+download_scripts
+chmod 777 $local_file 
+echo "Run readweb.sh in backgroud"
+exec $local_file&
+exit 0
