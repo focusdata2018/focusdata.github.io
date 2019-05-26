9ebc5b4682bce340e75ff53ec78b9cc9e06988f7
FocusData <kirillvz@yandex.ru>
2019-05-26 16:14:57 +0300

    Update

--- agent/senddata.sh
+++ agent/senddata.sh
@@ -65,7 +65,7 @@ do
 if [ "$received_size" -gt "50" ]; then
 echo -e "$m_date#from pipe1: \nreceived_size: $received_size"
 echo "$line">b64.txt
-echo "$line" | base64 --decode
+echo "$line" | tr \# \\n | base64 --decode > b64_decoded.txt
 else
 echo -e "$m_date#from pipe1: \n$line \nreceived_size: $received_size"
 fi
